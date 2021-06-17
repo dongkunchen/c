3e63b4ea4a031a8d2ff27897b26ccdb80da15eca
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 08:10:25 +0800

    Update simple1.c

--- simple1.c
+++ simple1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M 3
+#define N 3
 
 int main()
 {
 	int i;
-	int arr[M];
+	int arr[N];
 
 	printf("%d\n",sizeof(arr));
 
-	for(i = 0 ; i < M; i++)
+	for(i = 0 ; i < N; i++)
 		scanf("%d", &arr[i]);	
 
-	for(i = 0 ; i < M ; i++)
+	for(i = 0 ; i < N ; i++)
 		printf("%p --> %d\n",&arr[i],arr[i]);
 
 	exit(0);
