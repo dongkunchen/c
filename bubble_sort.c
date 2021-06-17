d866f55ff6fab38cdd13399868a2289bfdc88ddf
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 08:07:01 +0800

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -5,28 +5,28 @@
 
 static void sort(void)
 {
-    int a[N] = {12, 8, 45, 30, 98, 67, 2, 7, 68, 11};
+    int arr[N] = {12, 8, 45, 30, 98, 67, 2, 7, 68, 11};
     int i = 0;
     int j = 0;
     int tmp;
     for (i = 0; i < N; i++)
-        printf("%d ", a[i]);
+        printf("%d ", arr[i]);
     printf("\n");
 
     for (i = 0; i < (N - 1); i++)
     {
         for (j = 0; j < N - i - 1; j++)
         {
-            if (a[j] > a[j + 1])
+            if (arr[j] > arr[j + 1])
             {
-                tmp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = tmp;
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
             }
         }
     }
     for (i = 0; i < N; i++)
-        printf("%d ", a[i]);
+        printf("%d ", arr[i]);
     printf("\n");
 }
 
