7fd2def37cee9cc80d0c6e1c19b4f9232cc4c9f4
dongkunchen <g0933117131@gmail.com>
2021-06-22 07:03:09 +0800

    Update double_point.c

--- basic/pointer(linux)/double_point.c
+++ basic/pointer(linux)/double_point.c
@@ -5,10 +5,10 @@ int main()
 {
 	int i = 1;
 	int *p = &i;//類型(type)是int* 不是int
-    int **q = &p;
-
-	printf("%d\n", sizeof(i));//四個字節
-	//64位元/8 8字節 32位元/8 4字節
+ 	int **q = &p;
+	//int 4個字節
+	printf("%d\n", sizeof(i));
+	//pointer 64位元/8 8字節 32位元/8 4字節
 	printf("%d\n", sizeof(p));
 
 	//i的值可以 *p **p
@@ -20,10 +20,10 @@ int main()
 	printf("&p = %p\n", &p);		
 	printf("*p = %d\n", *p);
 
-    printf("q = %p\n", q);//p地址
-    printf("&q = %p\n", &q);//q地址
-    printf("*q = %p\n", *q);//i地址
-    printf("**q = %d\n", **q);//i的值
+        printf("q = %p\n", q);//p地址
+        printf("&q = %p\n", &q);//q地址
+        printf("*q = %p\n", *q);//i地址
+        printf("**q = %d\n", **q);//i的值
 
 	exit(0);
 }
